b6ee3a8040e7e0afcf63554888b16c585678d409
Joseph Ibochi <josephibochi@gmail.com>
2025-09-30 22:38:23 +0100

    chore: add env variables and config settings

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-
+from app.api.main import router
+from app.core.config import settings
 
 app = FastAPI(
-    title="NextGen Banking API",
-    description="API for banking operations",
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    docs_url=f"{settings.API_V1_STR}/docs",
+    redoc_url=f"{settings.API_V1_STR}/redoc",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
+app.include_router(router, prefix=settings.API_V1_STR)
 app.get("/health", tags=["Health"])(lambda: {"status": "ok"})
